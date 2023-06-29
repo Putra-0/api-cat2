f880ccab7e5a14e257dd0b3122ca2b7ea4f79900
Putra-0 <putrasa1219@gmail.com>
2023-06-29 09:48:05 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::middleware('role:user')->group(function () {
         Route::post('/transactions', [TransactionController::class, 'store']);
-        Route::get('/transactions/history', [TransactionController::class, 'history']);
+        Route::get('/transactions/history/{id}', [TransactionController::class, 'history']);
 
     });
 });
